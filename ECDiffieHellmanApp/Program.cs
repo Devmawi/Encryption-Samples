65e2de7cfbac1c70adfb04adc2847885ba1df33f
dev <dev@dev.org>
2022-02-07 22:15:00 +0100

    Add project files.

---
+++ ECDiffieHellmanApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace ECDiffieHellmanApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Let's Encrypt");
+            // .p8
+            //https://www.microfocus.com/documentation/visual-cobol/VC40/CSWin/BKCJCJCERTS001.html
+            var alice = new SecretMessageProcessor();
+            var bob = new SecretMessageProcessor();
+
+            var message = "Hello Encrypting";
+            var (iv, encodedMessage) = await alice.CreateEncodedMessageAsync(bob.PublicKey, message);
+            //var decodedMessage = await bob.DecodeMessage(alice.PublicKey, iv, encodedMessage);
+
+            Console.WriteLine($"Encoded message: {System.Text.Encoding.UTF8.GetString(encodedMessage)}");
+
+            var (publicKeyFile, privateKeyFile) = alice.ExportPublicPrivateKeyPair("Alice");
+            var publicKey = alice.ExportPublicKey();
+
+            var decodedMessageFromFile = await bob.DecodeMessage(publicKeyFile, iv, encodedMessage);
+            var decodedMessageFromKeyContent = await bob.DecodeMessage(publicKey, iv, encodedMessage);
+
+            Console.WriteLine($"Decoded message: {decodedMessageFromFile}");
+            // var susan = new SecretMessageProcessor();
+           // await susan.DecodeMessage(publicKey, iv, encodedMessage); Throws Exception
+        }
+    }
+}
---
+++ ECDiffieHellmanApp/SecretMessageProcessor.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECDiffieHellmanApp
+{
+    public class SecretMessageProcessor
+    {
+        public ECDiffieHellman ECDiffieHellman { get; set; } = ECDiffieHellman.Create();
+        public ECDiffieHellmanPublicKey PublicKey => ECDiffieHellman.PublicKey;
+
+        public async Task<(byte[], byte[])> CreateEncodedMessageAsync(ECDiffieHellmanPublicKey otherKey, string message)
+        {
+            var symkey = ECDiffieHellman.DeriveKeyMaterial(otherKey);
+            var aes = Aes.Create();
+            aes.Key = symkey;
+            aes.GenerateIV();
+
+            ICryptoTransform cryptoTransform = aes.CreateEncryptor();
+            MemoryStream ms = new();
+            using (CryptoStream cs = new CryptoStream(ms, cryptoTransform, CryptoStreamMode.Write))
+            {
+                var plainData = System.Text.Encoding.UTF8.GetBytes(message);
+                await cs.WriteAsync(plainData.AsMemory());
+            }
+            return (aes.IV, ms.ToArray());
+        }
+
+        public (string, string) ExportPublicPrivateKeyPair(string name)
+        {
+            ECDiffieHellman alg = ECDiffieHellman;
+            var (publicKey, privateKey) = (alg.ExportSubjectPublicKeyInfo(), alg.ExportPkcs8PrivateKey());
+
+            var publicKeyFile = Path.Combine(AppContext.BaseDirectory, $"{name}_PublicKey");
+            var privateKeyFile = Path.Combine(AppContext.BaseDirectory, $"{name}_PrivateKey");
+            File.WriteAllBytes(publicKeyFile, publicKey);
+            File.WriteAllBytes(privateKeyFile, privateKey);
+            return (publicKeyFile, privateKeyFile);
+        }
+
+        public byte[] ExportPublicKey()
+        {
+            return PublicKey.ToByteArray();
+        }
+
+        public async Task<string> DecodeMessage(ECDiffieHellmanPublicKey otherKey, byte[] iv, byte[] data)
+        {
+            
+            var symkey = ECDiffieHellman.DeriveKeyMaterial(otherKey);
+            var aes = Aes.Create();
+            aes.Key = symkey;
+            aes.IV = iv;
+
+            ICryptoTransform cryptoTransform = aes.CreateDecryptor();
+            MemoryStream ms = new();
+            using (CryptoStream cs = new CryptoStream(ms, cryptoTransform, CryptoStreamMode.Write))
+            {
+                await cs.WriteAsync(data.AsMemory());
+            }
+            return System.Text.Encoding.UTF8.GetString(ms.ToArray());
+        }
+
+        public async Task<string> DecodeMessage(byte[] publicKeyFile, byte[] iv, byte[] data)
+        {
+
+            var publicKeyFromFile = ECDiffieHellmanCngPublicKey.FromByteArray(publicKeyFile, CngKeyBlobFormat.EccPublicBlob);
+          
+             var symkey = ECDiffieHellman.DeriveKeyMaterial(publicKeyFromFile);
+            var aes = Aes.Create();
+            aes.Key = symkey;
+            aes.IV = iv;
+
+            ICryptoTransform cryptoTransform = aes.CreateDecryptor();
+            MemoryStream ms = new();
+            using (CryptoStream cs = new CryptoStream(ms, cryptoTransform, CryptoStreamMode.Write))
+            {
+                await cs.WriteAsync(data.AsMemory());
+            }
+            return System.Text.Encoding.UTF8.GetString(ms.ToArray());
+        }
+
+        public async Task<string> DecodeMessage(string publicKeyFile, byte[] iv, byte[] data)
+        {
+
+            var alg = ECDiffieHellman.Create();
+            alg.ImportSubjectPublicKeyInfo(File.ReadAllBytes(publicKeyFile), out int bytesRead);
+
+            var symkey = ECDiffieHellman.DeriveKeyMaterial(alg.PublicKey);
+            var aes = Aes.Create();
+            aes.Key = symkey;
+            aes.IV = iv;
+
+            ICryptoTransform cryptoTransform = aes.CreateDecryptor();
+            MemoryStream ms = new();
+            using (CryptoStream cs = new CryptoStream(ms, cryptoTransform, CryptoStreamMode.Write))
+            {
+                await cs.WriteAsync(data.AsMemory());
+            }
+            return System.Text.Encoding.UTF8.GetString(ms.ToArray());
+        }
+    }
+}
